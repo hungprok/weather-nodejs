f88ca247e12188f21fbb3fb9535d853eb1475481
hungprok <57722641+hungprok@users.noreply.github.com>
2020-04-15 11:45:56 +0700

    fix package json

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,20 +1,28 @@
-positionSuccess = (position) =>{
+console.log('hahahahahaha')
+// positionSuccess = (position) =>{
 
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    // var altitude = position.coords.altitude;
-    // var accuracy = position.coords.accuracy;
-    // var altitudeAccuracy = position.coords.altitudeAccuracy;
-    // var heading = position.coords.height;
-    // var speed = position.coords.speed;
-    // var timestamp = position.timestamp;
+//     var latitude = position.coords.latitude;
+//     var longitude = position.coords.longitude;
+//     // var altitude = position.coords.altitude;
+//     // var accuracy = position.coords.accuracy;
+//     // var altitudeAccuracy = position.coords.altitudeAccuracy;
+//     // var heading = position.coords.height;
+//     // var speed = position.coords.speed;
+//     // var timestamp = position.timestamp;
 
-    console.log(latitude);
-    console.log(longitude);
-}
+//     console.log(latitude);
+//     console.log(longitude);
+// }
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true });
-} else {
-    $(".map").text("Your browser is out of fashion, there's no geolocation!");
-}
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true });
+// } else {
+//     $(".map").text("Your browser is out of fashion, there's no geolocation!");
+// }
+
+navigator.geolocation.getCurrentPosition((coords) => {
+    console.log(coords)
+    var latitude = coords.coords.latitude;
+    var longitude = coords.coords.longitude;
+    console.log(latitude, longitude)
+});
